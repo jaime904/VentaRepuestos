fa6762ca134bef2418ba930f927fddd7c1e379d3
yt1706 <yari.toro@duocuc.cl>
2024-06-23 23:07:26 -0400

    prueba_yari

--- fronted/src/app/components/ListarVenta.jsx
+++ fronted/src/app/components/ListarVenta.jsx
@@ -7,7 +7,7 @@ export async function CargarVenta ()
     return data;
 }
 
-export async function ListarVenta ()
+export async function ListarVenta () //comentario de prueba
 {
     const date = await CargarVenta();
     console.log(date);
